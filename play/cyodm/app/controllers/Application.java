da6abfe458ba7026855547bd64ee4f096dd3e813
Ivan Fuyivara <ifuyivara@gmail.com>
2013-06-09 13:08:51 -0500

    adding initial testing

--- play/cyodm/app/controllers/Application.java
+++ play/cyodm/app/controllers/Application.java
@@ -1,14 +1,34 @@
 package controllers;
 
-import play.*;
+import play.libs.Json;
 import play.mvc.*;
 
-import views.html.*;
+import org.codehaus.jackson.JsonNode;
+import org.codehaus.jackson.node.ObjectNode;
 
 public class Application extends Controller {
   
     public static Result index() {
-        return ok(index.render("Your new application is ready."));
+        return ok("Your new application is ready.");
+    }
+    
+    @BodyParser.Of(BodyParser.Json.class)
+    public static Result sayTest() {
+      JsonNode json = request().body().asJson();
+      String name = json.findPath("name").getTextValue();
+      if(name == null) {
+        return badRequest("Missing parameter [name]");
+      } else {
+        return ok("Hello " + name);
+      }
+    }
+    
+    public static Result test(Long id) {
+    	ObjectNode result = Json.newObject();
+    	result.put("status", "OK");
+        result.put("message", "Get method for id = " + id);
+        return ok(result);
     }
   
 }
+
